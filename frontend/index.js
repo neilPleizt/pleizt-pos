ff073fa91681ba5a97c276ae551903e58ddd8b95
Neil David Sanchez Quintana <neil@pleizt.com>
2022-09-27 05:43:35 -0500

    babel setup

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,11 @@
-console.log('Worging Neil...')
+import React from "react";
+import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <h1>Hola Pleitz</h1>
+  </React.StrictMode>
+)
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,22 @@ module.exports = {
     filename: 'app.bundle.js'
   },
   plugins: [
-    new HtmlWebpackPlugin()
-  ]
+    new HtmlWebpackPlugin({
+      template: 'frontend/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
 }
