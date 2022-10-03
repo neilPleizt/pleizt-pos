f4487cb1405680f4aa48712c1006bd15a9846421
Neil Sanchez <neil.sanchez@bodytechcorp.com>
2022-10-02 22:08:24 -0500

    Modified Text

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <h1>Hola Pleitz</h1>
+    <h1>Hola Pleitz POS</h1>
   </React.StrictMode>
-)
+);
